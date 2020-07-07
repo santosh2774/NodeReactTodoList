79b2c3c3833742f6beb18cb365bdc1bc9e61f52c
santosh <santoshrumandla@gmail.com>
2020-07-07 19:11:01 +0530

    first commit

---
+++ build/precache-manifest.220bac3d96d6d2cfb7d11817fc577dee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467ff8edf9ad6ff0d354e896edf622b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09a634c1162714133170",
+    "url": "/static/css/main.46203649.chunk.css"
+  },
+  {
+    "revision": "52c168b84510317f6d8b",
+    "url": "/static/js/2.d61e99e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d61e99e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a634c1162714133170",
+    "url": "/static/js/main.c9365fc9.chunk.js"
+  },
+  {
+    "revision": "f8ce95e23ae4eb434ece",
+    "url": "/static/js/runtime-main.ef835f46.js"
+  }
+]);
