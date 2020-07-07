3246d4c4019cc16b7e5551c869fcc3ff8ce816ac
santosh <santoshrumandla@gmail.com>
2020-07-07 19:27:48 +0530

    client folder added

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 // app.use('/', indexRouter);
---
+++ client/build/precache-manifest.4d613f4de671615b0957df68a4032ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce095fe49f29c0bc69c9c4907165cc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4b7dc12579a810638e4",
+    "url": "/static/css/main.46203649.chunk.css"
+  },
+  {
+    "revision": "52c168b84510317f6d8b",
+    "url": "/static/js/2.d61e99e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d61e99e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b7dc12579a810638e4",
+    "url": "/static/js/main.d4290e7c.chunk.js"
+  },
+  {
+    "revision": "f8ce95e23ae4eb434ece",
+    "url": "/static/js/runtime-main.ef835f46.js"
+  }
+]);
